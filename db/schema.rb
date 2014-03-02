b41946cd4cacacfd10875f602fd8a0b721732283
flohdot <floh@flohdot.org>
2014-03-01 23:19:55 -0500

    stylin'

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20140120185410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "tickets", force: true do |t|
     t.string   "email"
