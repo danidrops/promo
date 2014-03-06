21cd97395301de7211c153121372cd08ed274281
Danielle Bakhazi <danielle.bakhazi@gmail.com>
2014-03-06 14:23:39 -0500

    exception notifier
    Signed-off-by: Danielle Bakhazi <danielle.bakhazi@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from Exception, :with => :server_error
+
+  def server_error(exception)
+  # Whatever code that handles the exception
+
+    ExceptionNotifier.notify_exception(exception,
+      :env => request.env)
+    # ExceptionNotifier::Notifier.exception_notification(request.env, exception).deliver
+  end
 end
