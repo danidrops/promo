12defbb5291a21065bd0950e37e6743d09d64e08
Danielle Bakhazi <danielle.bakhazi@gmail.com>
2014-03-05 21:05:44 -0500

    cleaning out charges (accidentally added I think)
    Signed-off-by: Danielle Bakhazi <danielle.bakhazi@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Promo::Application.routes.draw do
   resources :tickets
-  resources :charges
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
