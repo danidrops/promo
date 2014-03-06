624a70a7607e743da8ffa680d704a00d4cf20f11
Danielle Bakhazi <danielle.bakhazi@gmail.com>
2014-03-06 03:24:30 -0500

    Configured email with redacted password. Emails now go out (with attachments and all!)
    Signed-off-by: Danielle Bakhazi <danielle.bakhazi@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,9 +61,19 @@ Promo::Application.configure do
   # application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  #mailer stuff
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'trystmtl@gmail.com',
+    password:             'REDACTED',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
