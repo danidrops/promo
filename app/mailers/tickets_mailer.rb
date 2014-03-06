db24a679db514be349b67e258636f9817f8f5245
Danielle Bakhazi <danielle.bakhazi@gmail.com>
2014-03-06 13:58:49 -0500

    Put in the real ticket template and positioned ticketNo. accordingly
    Signed-off-by: Danielle Bakhazi <danielle.bakhazi@gmail.com>

--- app/mailers/tickets_mailer.rb
+++ app/mailers/tickets_mailer.rb
@@ -9,8 +9,8 @@ class TicketsMailer < ActionMailer::Base
     @ticket.number.times do
       stub_code = @ticket.id.to_s + "-" + incr.to_s
       Prawn::Document.generate("public/stubs/TrystTicketNo#{stub_code}.pdf") do
-        image backgroundpath, :width => 500, :height => 300
-        draw_text stub_code, :at => [170,480], :size => 20
+        image backgroundpath
+        draw_text stub_code, :at => [250,265], :size => 20
       end
       attachments["TrystTicketNo#{stub_code}.pdf"] = File.read("public/stubs/TrystTicketNo#{stub_code}.pdf")
       Stub.create({code: stub_code, ticket_id: @ticket.id})
