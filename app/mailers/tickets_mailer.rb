d85740773a5b1a7c2e547b574371c8d5c9e431dd
Danielle Bakhazi <danielle.bakhazi@gmail.com>
2014-03-07 14:04:36 -0500

    avoid adding a file to public on heroku. prawn generate directly into attachment.
    Signed-off-by: Danielle Bakhazi <danielle.bakhazi@gmail.com>

--- app/mailers/tickets_mailer.rb
+++ app/mailers/tickets_mailer.rb
@@ -8,11 +8,10 @@ class TicketsMailer < ActionMailer::Base
     incr = 1
     @ticket.number.times do
       stub_code = @ticket.id.to_s + "-" + incr.to_s
-      Prawn::Document.generate("public/stubs/TrystTicketNo#{stub_code}.pdf") do
+      attachments["TrystTicketNo#{stub_code}.pdf"] = Prawn::Document.generate() do
         image backgroundpath
         draw_text stub_code, :at => [250,265], :size => 20
       end
-      attachments["TrystTicketNo#{stub_code}.pdf"] = File.read("public/stubs/TrystTicketNo#{stub_code}.pdf")
       Stub.create({code: stub_code, ticket_id: @ticket.id})
       incr+=1
     end
