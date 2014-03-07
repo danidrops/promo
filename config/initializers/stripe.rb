c78acf278740fccdece2bcb7b2cc675da6f156fc
Danielle Bakhazi <danielle.bakhazi@gmail.com>
2014-03-07 15:16:57 -0500

    stripe config'd live keys with env variables
    Signed-off-by: Danielle Bakhazi <danielle.bakhazi@gmail.com>

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,12 @@
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_cjuuAiA9AEchN2sJCZioUUrX',
-  :secret_key      => 'sk_test_OHkA9sxF25wTR4dOV1OcsNtW'
-}
-
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    :publishable_key => ENV["STRIPE_PKEY"] ,
+    :secret_key      => ENV["STRIPE_SKEY"]
+  }
+else
+  Rails.configuration.stripe = {
+    :publishable_key => 'pk_test_cjuuAiA9AEchN2sJCZioUUrX',
+    :secret_key      => 'sk_test_OHkA9sxF25wTR4dOV1OcsNtW'
+  }
+end
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
