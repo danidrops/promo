9dcbb29eebf34bd2e86c00904a3283e824133fd7
Danielle Bakhazi <danielle.bakhazi@gmail.com>
2014-03-07 16:15:39 -0500

    https redirect
    Signed-off-by: Danielle Bakhazi <danielle.bakhazi@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_filter :https_redirect
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   rescue_from Exception, :with => :server_error
 
   def server_error(exception)
@@ -12,4 +12,15 @@ class ApplicationController < ActionController::Base
       :env => request.env)
     # ExceptionNotifier::Notifier.exception_notification(request.env, exception).deliver
   end
+
+  private
+
+    def https_redirect
+      if Rails.env.production?
+        if !request.ssl?
+          flash.keep
+          redirect_to protocol: "https://", status: :moved_permanently
+        end
+      end
+    end
 end
