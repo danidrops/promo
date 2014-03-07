1567c651d289c93695093f67b965fa5320e0ba6f
Danielle Bakhazi <danielle.bakhazi@gmail.com>
2014-03-07 13:40:43 -0500

    preparing to test live with stripe TEST
    Signed-off-by: Danielle Bakhazi <danielle.bakhazi@gmail.com>

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -27,7 +27,7 @@ class TicketsController < ApplicationController
       :currency    => 'cad'
     )
 
-    @ticket = Ticket.create({ email: @email, number: @number, note: "online $8"}) #CHANGE TICKET PRICE NOTE HERE
+    @ticket = Ticket.create({ email: @email, number: @number, note: "heroku test"}) #CHANGE TICKET PRICE NOTE HERE
     TicketsMailer.ticket_sender(@ticket).deliver
 
 
