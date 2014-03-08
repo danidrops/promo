af40272292dc25d3e970d4e68a66e75cba661541
Danielle Bakhazi <danielle.bakhazi@gmail.com>
2014-03-07 21:38:57 -0500

    only index routes for stubs
    Signed-off-by: Danielle Bakhazi <danielle.bakhazi@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Promo::Application.routes.draw do
   resources :tickets
-  resources :stubs
+  resources :stubs, only: [:index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
